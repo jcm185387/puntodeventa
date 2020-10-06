b0ef287ed204c5686c01d0b26e0d43f9b49872a4
Juan Cruz Mogica <juan.cruz@neoris.com>
2020-10-05 19:35:06 -0500

    Pequeños cambios en la vista de productos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -2,10 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Proyectos</h5>
+
+                <p class="card-text">
+                    <span>En esta sección podremos agregar todos los productos que se agreguen al inventario</span>                    
+                </p>
+                <div class="col-12">
+                    <div class="pull-right" style="float: right;">
+                    <strong>Nuevo producto</strong>
+                    <button @submit.prevent="Agregar()" class="btn  btn-primary" id="AddProduct">+ <i class="fas fa-folder"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+    <div class="row justify-content-center" style="margin-top : 5%">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Categorías</div>
+                <div class="card-header">Productos</div>
                 <div class="card-body">    
                     <productos-component></productos-component>                    
                 </div>
